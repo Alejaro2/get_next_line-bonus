ab156cc49d2de19de5f0bf9865a803ebc50b9d77
Alejandro Rodriguez <alejaro2@c2r9s1.42madrid.com>
2024-12-19 17:45:27 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char *get_next_line(int fd);
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejaro2 <alejaro2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 19:58:20 by alejaro2          #+#    #+#             */
+/*   Updated: 2024/12/19 17:42:04 by alejaro2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len;
+	char	*p;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	p = malloc(len + 1);
+	if (!p)
+		return (NULL);
+	ft_strlcpy(p, s1, ft_strlen(s1) + 1);
+	ft_strlcat(p, s2, len + 1);
+	free((char *)s1);
+	return (p);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (src && src[j] != '\0')
+		j++;
+	if (size > 0)
+	{
+		while (i < size - 1 && src && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (j);
+}
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (str == NULL)
+		return(0);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	lens;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	lens = src_len + dst_len;
+	if (size <= dst_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] && dst_len + i < size - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (lens);
+}
+
